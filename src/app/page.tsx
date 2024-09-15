1b7d44b2318cb5ab22aff90822cff2001158e9dd
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-09-15 21:52:56 +0700

    refactor: refactor loading and baseurl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,25 @@
 import Navbar from "@/components/navbar";
 import HomeContent from "./components/home-content";
 import { Suspense } from "react";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <div className="flex min-h-svh w-full items-center justify-center bg-white dark:bg-zinc-800">
+          <div className="relative h-20 w-auto">
+            <Image
+              src={"/assets/image/svg"}
+              className="object-contain"
+              sizes="100%"
+              fill
+              alt="logo-loading"
+            />
+          </div>
+        </div>
+      }
+    >
       <Navbar />
       <HomeContent />
     </Suspense>
